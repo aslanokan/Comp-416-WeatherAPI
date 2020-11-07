78fdcd171b1be664256d104e24967841487b5161
Gül Sena Altıntaş <galtintas17@ku.edu.tr>
2020-11-08 00:03:24 +0300

    Added mode to server, Added authentication files for 5 users

--- app/src/main/java/Comp/WeatherAPI/Server/ServerThread.java
+++ app/src/main/java/Comp/WeatherAPI/Server/ServerThread.java
@@ -7,16 +7,52 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 class ServerThread extends Thread {
+    public static final int DEFAULT_DATA_PORT = 4443;
 
     protected BufferedReader inputStream;
     protected PrintWriter outputStream;
     protected Socket socket;
+    protected int mode;
     private String line = "";
     private String lines = "";
     private Authentication auth;
 
-    public ServerThread(Socket socket) {
+    public ServerThread(Socket socket, int mode) {
         this.socket = socket;
+        if (mode != 1 && mode != 0)
+            mode=0;
+        this.mode=mode;
+    }
+
+    private void authenticate() throws IOException {
+        auth = Authentication.getInstance();
+        line = inputStream.readLine();
+
+        if (! line.startsWith(AuthenticationMessages.Auth_Request.toString())) {
+            outputStream.println(AuthenticationMessages.Auth_Fail + AuthenticationMessages.DELIMITER + "wrong header.");
+            outputStream.flush();
+            return;
+        }
+        String[] linesValues = line.split(AuthenticationMessages.DELIMITER);
+        String username = linesValues[linesValues.length - 1];
+
+        if (line.compareTo("QUIT") != 0) {
+            if (!auth.validateUser(username)) {
+                outputStream.println(AuthenticationMessages.Auth_Fail + AuthenticationMessages.DELIMITER + "User doesn't exist.");
+                outputStream.flush();
+                return;
+            }
+            Authentication.AuthenticationResult authRes = auth.authorize(username, this);
+            boolean authenticated = authRes.authenticated;
+            outputStream.println(authRes.result + AuthenticationMessages.DELIMITER + authRes.customMessage);
+            outputStream.flush();
+
+            if (!authenticated) {
+                return;
+            } else {
+                Server dataServer = new Server(DEFAULT_DATA_PORT, 1);
+            }
+        }
     }
 
     public void run() {
@@ -28,35 +64,12 @@ class ServerThread extends Thread {
         }
 
         try {
-            auth = Authentication.getInstance();
-            line = inputStream.readLine();
-
-            String[] linesValues = line.split(AuthenticationMessages.DELIMITER);
-            String username = linesValues[linesValues.length - 1];
-
-            if (line.compareTo("QUIT") != 0) {
-                if (!auth.validateUser(username)) {
-                    outputStream.println(AuthenticationMessages.Auth_Fail + AuthenticationMessages.DELIMITER + "User doesn't exist.");
-                    outputStream.flush();
-                    return;
-                }
-                Authentication.AuthenticationResult authRes = auth.authorize(username, this);
-                boolean authenticated = authRes.authenticated;
-                outputStream.println(authRes.result + AuthenticationMessages.DELIMITER + authRes.customMessage);
+            if (this.mode==0) {
+                authenticate();
+            } else {
+                // query phase
+                outputStream.println("Welcome to data socket");
                 outputStream.flush();
-
-                if (!authenticated) {
-                    return;
-                } else {
-                    outputStream.println(AuthenticationMessages.Auth_Success + AuthenticationMessages.DELIMITER);
-                    while (line.compareTo("QUIT") != 0) {
-                        lines = "Client messaged : " + line + " at  : " + Thread.currentThread().getId();
-                        outputStream.println(lines);
-                        outputStream.flush();
-                        System.out.println("Client " + socket.getRemoteSocketAddress() + " sent :  " + lines);
-                        line = inputStream.readLine();
-                    }
-                }
             }
         } catch (IOException e) {
             line = this.getName();
